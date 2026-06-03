571fd2edf181177dbf4c1d3b3c501e075da47ce2
Felix Schmid <50250674+Felix-Schmid@users.noreply.github.com>
2026-06-03 15:23:07 +0200

    Further CSS cleanup

--- OICtimetable.js
+++ OICtimetable.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @require      https://unpkg.com/ical.js@2.1.0/dist/ical.es5.min.cjs
 // @require      https://cdn.jsdelivr.net/npm/chart.js
-// @version      2026-05-15
+// @version      2026-06-03
 // @description  Make it easy to check which rooms are booked at the JKU OIC.
 // @author       Felix Schmid
 // @match        https://gwcal.jku.at/gw/webacc
@@ -131,12 +131,12 @@ dialog {
 	padding: 8px;
 }
 
-.svgBtn {
-	padding: 0;
+button > svg {
+	vertical-align: middle 1px;
 }
 
-.svgBtn > svg {
-	vertical-align: middle;
+#prevDayBtn, #nextDayBtn {
+	padding: 0;
 }
 
 #refreshBtn {
@@ -158,10 +158,6 @@ dialog {
 	box-shadow: 0 0 1px 0 rgba(24, 94, 224, 0.15), 0 6px 12px 0 rgba(24, 94, 224, 0.15);
 	gap: 4px;
 	padding: 4px;
-}
-
-.tab svg {
-	vertical-align: middle;
 }
 
 button {
@@ -314,13 +310,13 @@ th > small {
 	<div id="datepanel">
 		<h1>OIC Timetable</h1>
 
-		<button id="prevDayBtn" class="svgBtn" aria-label="previous day"><svg width="28px" height="31px" viewBox="0 -960 960 960"><path d="M560-280 360-480l200-200v400Z"/></svg></button>
+		<button id="prevDayBtn" aria-label="previous day"><svg width="28px" height="31px" viewBox="0 -960 960 960"><path d="M560-280 360-480l200-200v400Z"/></svg></button>
 		<input type="date" id="dateinput" name="date input"/>
-		<button id="nextDayBtn" class="svgBtn" aria-label="next day"><svg width="28px" height="31px" viewBox="0 -960 960 960"><path d="M400-280v-400l200 200-200 200Z"/></svg></button>
+		<button id="nextDayBtn" aria-label="next day"><svg width="28px" height="31px" viewBox="0 -960 960 960"><path d="M400-280v-400l200 200-200 200Z"/></svg></button>
 
 		<div style="flex-grow: 1"></div>
-		<button id="refreshBtn" class="svgBtn" aria-label="refresh"> <svg width="24px" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q-32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t-196 67Z"/></svg> Updated: <em id="lastRefresh"></em> </button>
-		<button id="infoBtn" class="svgBtn" aria-label="info"> <svg width="24px" height="24px" viewBox="0 -960 960 960"><path d="M440-280h80v-240h-80v240Zm68.5-331.5Q520-623 520-640t-11.5-28.5Q497-680 480-680t-28.5 11.5Q440-657 440-640t11.5 28.5Q463-600 480-600t28.5-11.5ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg> </button>
+		<button id="refreshBtn" aria-label="refresh"> <svg width="24px" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q-32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t-196 67Z"/></svg> Updated: <em id="lastRefresh"></em> </button>
+		<button id="infoBtn" aria-label="info"> <svg width="24px" height="24px" viewBox="0 -960 960 960"><path d="M440-280h80v-240h-80v240Zm68.5-331.5Q520-623 520-640t-11.5-28.5Q497-680 480-680t-28.5 11.5Q440-657 440-640t11.5 28.5Q463-600 480-600t28.5-11.5ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg> </button>
 	</div>
 
 	<div id="inputerror" class="errorcontainer">
