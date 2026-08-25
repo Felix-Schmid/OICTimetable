6a1e77bfb0edbc2ea9333b6a039f34b8eea45469
Felix Schmid <50250674+Felix-Schmid@users.noreply.github.com>
2026-08-25 16:27:09 +0200

    Add info screen page for single room

---
+++ server/common.js
@@ -0,0 +1,93 @@
+const rooms = {
+	"merkur": {"name": "Merkur", "building": "Erdgeschoss", "capacity": 4, "color": "#8c8a89"},
+	"venus": {"name": "Venus", "building": "Erdgeschoss", "capacity": 4, "color": "#dab292"},
+	"erde": {"name": "Erde", "building": "Erdgeschoss", "capacity": 4, "color": "#6288a8"},
+	"mars": {"name": "Mars", "building": "Erdgeschoss", "capacity": 4, "color": "#f27c5f"},
+	"jupiter": {"name": "Jupiter", "building": "Erdgeschoss", "capacity": 10, "color": "#c08137"},
+	"saturn": {"name": "Saturn", "building": "Erdgeschoss", "capacity": 10, "color": "#dab778"},
+	"uranus": {"name": "Uranus", "building": "Erdgeschoss", "capacity": 10, "color": "#95bbbe"},
+	"neptun": {"name": "Neptun", "building": "Erdgeschoss", "capacity": 10, "color": "#7595bf"},
+	"bumblebee": {"name": "Bumblebee", "building": "1. Obergeschoss", "capacity": 10, "color": "#debd45"},
+	"eve": {"name": "EVE", "building": "1. Obergeschoss", "capacity": 10, "color": "#4e79e8"},
+	"optimus-prime": {"name": "Optimus-Prime", "building": "1. Obergeschoss", "capacity": 4, "color": "#d04a4a"},
+	"seminar-room": {"name": "Seminar-room", "building": "1. Obergeschoss", "capacity": 20, "color": "#54a348"},
+	"wall-e": {"name": "WALL·E", "building": "1. Obergeschoss", "capacity": 10, "color": "#d9884a"}
+};
+const baseUrl = "room/";
+
+function getBookingsFromICAL(icalData) {
+	const bookings = {};
+	const parseRes = ICAL.parse(icalData);
+	const comp = new ICAL.Component(parseRes);
+	const vevents = comp.getAllSubcomponents("vevent");
+
+	vevents.forEach(event => {
+		addBookingEvent(event, bookings);
+	});
+	return bookings;
+}
+
+function addBookingEvent(event, bookingsData) {
+	const date = new Date();
+	date.setFullYear(date.getFullYear() + 1); // expand max 1 year into future
+	const rangeEnd = ICAL.Time.fromJSDate(date);
+	const start = event.getFirstPropertyValue("dtstart")
+
+	const expand = new ICAL.RecurExpansion({
+		component: event,
+		dtstart: start
+	});
+
+	let expanded = false;
+	let next; // next is always an ICAL.Time or null
+	while (next = expand.next()) {
+		expanded = true;
+		if (next.compare(rangeEnd) > 0) {
+			break;
+		}
+		addBookingTime(next, event, bookingsData);
+	}
+	if (!expanded) {
+		addBookingTime(start, event, bookingsData);
+	}
+}
+
+function addBookingTime(time, event, bookingsData) {
+	const current = time.toJSDate();
+	const summary = event.getFirstPropertyValue("summary");
+
+	const start = event.getFirstPropertyValue("dtstart").toJSDate();
+	const end = event.getFirstPropertyValue("dtend").toJSDate();
+	let duration = (end - start) / (1000 * 60); // ms to minutes
+
+	let currentEnd = new Date(current);
+	currentEnd = currentEnd.setMinutes(currentEnd.getMinutes() + duration);
+
+	while (current < currentEnd) {
+		const offset = current.getHours() * 60 + current.getMinutes();
+		const durationDay = Math.min(duration, 24 * 60 - offset); // clamp to end of current day
+		duration -= durationDay;
+
+		const dateKey = dateToString(current);
+		if (bookingsData[dateKey] === undefined) {
+			bookingsData[dateKey] = [];
+		}
+		bookingsData[dateKey].push(
+			{"o": offset, "d": durationDay, "s": summary}
+		);
+		
+		current.setHours(0, 0, 0, 0); // always begins at midnight for next days
+		current.setDate(current.getDate() + 1);
+	}
+}
+
+/** takes a time in minutes from midnight and returns a string in format hh:mm */
+function minutesToClock(time) {
+	const min = (time % 60).toString().padStart(2, "0");
+	return Math.floor(time / 60) + ":" + min;
+}
+
+/** takes a Date object and returns a string in the format "YYYY-MM-DD" */
+function dateToString(date) {
+	return date.toISOString().split('T')[0]
+}
---
+++ server/screen.js
@@ -0,0 +1,137 @@
+let bookings = {};
+
+// ==== html elements
+let roomName;
+let currentName;
+let currentTime;
+let eInkDisplay;
+let upcomingList;
+
+window.onload = function() {
+	init();
+};
+
+function init() {
+	roomName = document.getElementById("room-name");
+	currentName = document.getElementById("current-name");
+	currentTime = document.getElementById("current-time");
+	eInkDisplay = document.getElementById("e-ink-display");
+	upcomingList = 	document.getElementById("upcoming-list");
+	onhashchange = () => { refreshData() };
+	refreshData();
+}
+
+/** fetch data from OIC, and refresh the screen */
+function refreshData() {
+	bookings = {}; // clear any previous data
+	
+	let hash = window.location.hash;
+	if (hash) {
+		hash = hash.substring(1); // remove "#"
+	}
+	if (!hash || !(hash in rooms)) { // hash was empty or invalid
+		clearScreen();
+		roomName.innerText = "[ERROR] Invalid room ID";
+		return;
+	}
+	roomName.innerText = rooms[hash].name;
+
+	fetch(baseUrl + hash)
+		.then(response => {
+			if (response.ok) {
+				return response.text();
+			}
+			throw new Error("Could not fetch data");
+		})
+		.then(data => {
+			bookings = getBookingsFromICAL(data);
+			refreshScreen();
+		})
+		.catch((error) => {
+			clearScreen();
+			roomName.innerText = "[ERROR] Could not fetch data";
+			console.warn(error);
+		});
+}
+
+/** refresh the info screen with booking data for the current time and date */
+function refreshScreen() {
+	const now = new Date();
+	const minutes = now.getHours() * 60 + now.getMinutes();
+
+	let bookingsDay = bookings[dateToString(now)];
+	if (bookingsDay == undefined || Object.keys(bookingsDay).length === 0) { // days with no slots are empty objects
+		bookingsDay = [];
+	}
+
+	let upcomingEvents = bookingsDay
+		.slice() // create a shallow copy
+		.sort((a, b) => a.o - b.o) // sort by event offset
+		.filter(event => (event.o + event.d) >= minutes) // remove past events
+		.slice(0, 3); // take first 3
+
+	let currentEvent = null;
+	let upcoming = [...upcomingEvents]; // create a copy to work with
+
+	if (upcoming.length > 0 && upcoming[0].o < minutes) {
+		currentEvent = upcoming.shift(); // remove current from upcoming
+	}
+
+	// set title of current
+	if (currentEvent == null) {
+		currentName.innerText = "FREE";
+		eInkDisplay.classList = ["is-free"];
+
+		if (upcoming.length == 0) {
+			currentTime.innerText = "For Today";
+		} else {
+			currentTime.innerText = "Until " + minutesToClock(upcoming[0].o);
+		}
+	} else {
+		eInkDisplay.classList = ["is-busy"];
+		setEventTitle(currentEvent, currentName);
+		setEventTime(currentEvent, currentTime);
+	}
+
+	// set upcoming section
+	upcomingList.replaceChildren(); // clear
+	for (let i = 0; i < Math.min(upcoming.length, 2); i++) {
+		const event = upcoming[i];
+
+		const li = document.createElement("li");
+		const nameSpan = document.createElement("span");
+		nameSpan.className = "meeting-name";
+		const timeSpan = document.createElement("span");
+		timeSpan.className = "meeting-time";
+
+		setEventTitle(event, nameSpan);
+		setEventTime(event, timeSpan);
+
+		li.appendChild(timeSpan);
+		li.appendChild(nameSpan);
+		upcomingList.appendChild(li);
+	}
+	if (upcoming.length == 0) {
+		upcomingList.innerHTML = "<li><i><small>No further meetings are currently scheduled for today.</small></i></li>";
+	}
+}
+
+/** remove all booking data from the info screen */
+function clearScreen() {
+	currentName.innerText = "";
+	currentTime.innerText = "";
+	eInkDisplay.classList = ["is-free"];
+	upcomingList.innerHTML = "-";
+}
+
+function setEventTitle(event, nameElem) {
+	if (event.s && event.s.trim()) {
+		nameElem.innerText = event.s; // use innerText to avoid injection
+	} else {
+		nameElem.innerHTML = "<i>No Title</i>"; // use innerHTML for styled string
+	}
+}
+
+function setEventTime(event, timeElem) {
+	timeElem.innerText = minutesToClock(event.o) + " - " + minutesToClock(event.o + event.d);
+}
